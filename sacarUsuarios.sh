b70427451cb6e724f67e06590ebf76a92ba37b3d
juanamortegui <83096174+juanamortegui@users.noreply.github.com>
2023-03-08 12:41:45 -0600

    Update sacarUsuarios.sh

--- sacarUsuarios.sh
+++ sacarUsuarios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Juan Camilo Amortegui Salazar
 
-#Programa que saca los nombres de usuario y el directorio home de cada uno dependiendo de si la sheel esta configurada en /bin/false
+#Programa que saca los nombres de usuario y el directorio home de cada uno dependiendo de si la shell esta configurada en /bin/false
 #En el if tambien sirve poner "$linea" | grep "/bin/false" > /dev/null
 while read -r linea; do
     if echo "$linea" | grep -q "/bin/false"; then
