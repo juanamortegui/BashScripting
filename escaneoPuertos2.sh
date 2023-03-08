171dae2b7d53d49d66f5fd604e617a68f2100bfb
juanamortegui <83096174+juanamortegui@users.noreply.github.com>
2023-03-08 12:05:05 -0500

    Add files via upload

---
+++ escaneoPuertos2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Juan Camilo Amortegui Salazar
+
+#Programa que escanea los puertos de un host dado usando (nping <ip> -p <puertos> -c1 --rate <#_peticiones/seg>)
+#Tambien se puede usar el comando (nc -zvvvn <ip> <puertos>)
+
+#Parametros de entrada
+ip=$1
+puertos=$2
+
+#Vriables
+rate=20
+seg_parametro=${#puertos}
+
+echo "Escaneando..."
+
+function escanear_1000_puertos_comunes(){
+    rate_all=$((rate * 30))
+    puerto=$(cat /home/juan/Desktop/bashScripts/puerto.txt)
+    resultado=$(nping $ip -p $puerto -c1 --rate $rate_all)
+    imprimir
+}
+
+function escanear_unicos_puertos(){
+    resultado=$(nping $ip -p $puertos -c1 --rate $rate )
+    imprimir
+}
+
+function imprimir(){
+    echo "$resultado" | grep -i "completed" | awk -F " " '{print $5}' | awk -F ":" '{print "PUERTO: "$2"/TCP - ABIERTO"}'
+}
+if (nping $ip | grep -q "No route to host") ;then
+    echo "No se puede conectar al Host"
+else
+    if [ $seg_parametro = 0 ]; then 
+        escanear_1000_puertos_comunes
+    else 
+        escanear_unicos_puertos
+    fi
+fi
