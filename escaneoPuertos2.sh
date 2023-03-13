5ca1af5f39833f9bcdbe3fc525604bbcce17f60f
juanamortegui <83096174+juanamortegui@users.noreply.github.com>
2023-03-13 12:28:49 -0500

    Update escaneoPuertos2.sh

--- escaneoPuertos2.sh
+++ escaneoPuertos2.sh
@@ -10,9 +10,8 @@ puertos=$2
 
 #Constantes
 rate=20
-directorio=.escaneoPuertos
-ruta_inicial=$(pwd)
-archivo=4172636869766F20646520313030302070756572746F73.txt
+archivo=.4172636869766F20646520313030302070756572746F73.txt
+url=https://raw.githubusercontent.com/juanamortegui/BashScripting/main/.4172636869766F20646520313030302070756572746F73.txt
 
 #Variables
 seg_parametro=${#puertos}
@@ -20,12 +19,11 @@ seg_parametro=${#puertos}
 echo "Escaneando..."
 
 function escanear_1000_puertos_comunes(){
-    if [[ ! -e $archivo ]] ; then
-        wget -q https://raw.githubusercontent.com/juanamortegui/BashScripting/main/4172636869766F20646520313030302070756572746F73.txt
+    if [[ ! -e $HOME/$archivo ]] ; then
+        wget -P $HOME -q $url
     fi
     rate_all=$((rate * 30))
-    mil_puertos=$(cat $archivo)
-    cd $ruta_inicial
+    mil_puertos=$(cat $HOME/$archivo)
     resultado=$(nping $ip -p $mil_puertos -c1 --rate $rate_all)
     imprimir
 }
@@ -43,13 +41,6 @@ if (nping $ip | grep -q "No route to host") ;then
     echo "No se puede conectar al Host"
 else
     if [ $seg_parametro = 0 ]; then 
-    cd /home/$USER
-    if [ ! -d  $directorio ]; then
-        mkdir $directorio
-        chmod 700 $directorio
-    fi
-    cd $directorio
-    ruta_directorio=$(pwd)
         escanear_1000_puertos_comunes
     else 
         escanear_unicos_puertos
